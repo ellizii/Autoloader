bf56cfc5578d6c6b3805f5fab645f3b798039c3c
elliz <ellizii@mail.ru>
2021-09-05 19:37:29 +0300

    Create autoloader

--- examples/autoloader.php
+++ examples/autoloader.php
@@ -5,7 +5,7 @@ use Ellizii\Autoloader\Autoloader;
 include_once dirname(__DIR__) . '/src/Autoloader.php';
 /*===============================================*/
 /* folder */
-//$path =array(__DIR__.'/load');
+$path =__DIR__.'/load';
 
 /* files array */
 //$path =array(__DIR__.'/load/Load.php',__DIR__.'/load/LoadTrait.php');
@@ -13,7 +13,7 @@ include_once dirname(__DIR__) . '/src/Autoloader.php';
 /* file */
 //$path = __DIR__.'/load/LoadInterface.php';
 
-$path = __DIR__.'/load/loadArray.php';
+//$path = __DIR__.'/load/loadArray.php';
 
 $loader =  Autoloader::instance($path);
 
